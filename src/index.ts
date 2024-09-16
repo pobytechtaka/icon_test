5f551a2a2997ceda7ccd6f3d3e3e3c0ac2896529
GitHub Action <action@github.com>
2024-09-16 15:28:02 +0000

    Auto-commit after running npm script

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
+import CircleBox24 from "./CircleBox24";
+import Clear24 from "./Clear24";
+import Copy24 from "./Copy24";
+import Flag24 from "./Flag24";
 import Mail24 from "./Mail24";
+import Persion24 from "./Persion24";
+import Pin24 from "./Pin24";
 import Plus24 from "./Plus24";
+import Save24 from "./Save24";
 import Scissors24 from "./Scissors24";
 
+export { default as CircleBox24 } from "./CircleBox24";
+export { default as Clear24 } from "./Clear24";
+export { default as Copy24 } from "./Copy24";
+export { default as Flag24 } from "./Flag24";
 export { default as Mail24 } from "./Mail24";
+export { default as Persion24 } from "./Persion24";
+export { default as Pin24 } from "./Pin24";
 export { default as Plus24 } from "./Plus24";
+export { default as Save24 } from "./Save24";
 export { default as Scissors24 } from "./Scissors24";
 
 export default {
+  CircleBox24,
+  Clear24,
+  Copy24,
+  Flag24,
   Mail24,
+  Persion24,
+  Pin24,
   Plus24,
+  Save24,
   Scissors24,
 };
   
